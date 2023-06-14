323180ee095e6239c33d3c23fdb7c11d76da5659
Iqbal Farooqui <iqbal.farooqui@zillisor.com>
2023-06-14 09:51:58 -0400

    Init commit

---
+++ src/main/java/com/example/demo/DependabotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabotApplication.class, args);
+	}
+
+}
